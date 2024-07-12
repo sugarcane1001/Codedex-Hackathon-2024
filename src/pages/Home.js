c13202299ed12f434f7a5d9b5439c458c9605d60
Zian <hasanzian6@gmail.com>
2024-07-12 10:46:53 -0400

    Resolved merge conflict in Menu.css by accepting current changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,50 @@
-
 import React from 'react';
-import '../assets/css/Home.css'
+import '../assets/css/Home.css';
 
 const Home = () => {
   return (
     <div className="home-container">
-    <div className="horizontal-box">
-      <h1>OUR STORY</h1>
-      Founder, Jonathan Li, shares a passion for board games, boba, and delicious food, so he combined them all to become Sip & Play, Park Slope’s 
-        first board game cafe.  It is a straightforward concept, come in with your friends and family to play any board game from our library of 300+ games!
-         We hope when you visit, you also enjoy our coffee, espresso, boba, sandwiches, and snacks!
-    </div>
-    <div className="vertical-boxes">
-      <div className="vertical-box">
-        <h1> BIRTHDAY PACKAGES </h1>
-        <b>Renting the Entire Backroom:</b> <br></br>
-        $350 for 3 hours of game time all to your party! <br></br>
-        <br></br>
 
-        <b>Birthday Backroom Packages:</b>
-        <ul>
-            <li>
-            $550 Birthday basic package. Have the entire backroom to yourselves and get pizza and soda for the whole party! For ten people, each additional person is +$30.
-            </li>
-            <li>
-            $700 Birthday premium package. Have the entire backroom to yourselves with pizza, soda, unlimited bubble tea, unlimited appetizers and each participant gets a Sip & Play goodie bag! For ten people, each additional person is +$45
-            </li>
-        </ul>
+      <div className="vertical-boxes">
+        <div className="vertical-box our-story">
+          <h1>OUR STORY</h1>
+          <p>
+            Founder, Jonathan Li, shares a passion for board games, boba, and delicious food, so he combined them all to become Sip & Play, Park Slope’s first board game cafe. It is a straightforward concept, come in with your friends and family to play any board game from our library of 300+ games! We hope when you visit, you also enjoy our coffee, espresso, boba, sandwiches, and snacks!
+          </p>
+        </div>
+        <div className="vertical-box birthday-package">
+          <h1>BIRTHDAY PACKAGES</h1>
+          <p>
+            <b>Renting the Entire Backroom:</b> <br />
+            $350 for 3 hours of game time all to your party! <br /><br />
 
-        <i>For any other large party inquiries, click the “Reserve Now!” button.</i>
-      </div>
-      <div className="vertical-box">
-        <h1>PRICING</h1>
-        <ul>
+            <b>Birthday Backroom Packages:</b>
+          </p>
+          <ul>
             <li>
-            <b>$10</b> per person for 3 hours (<b>$12</b> on weekends and holidays)            
+              $550 Birthday basic package. Have the entire backroom to yourselves and get pizza and soda for the whole party! For ten people, each additional person is +$30.
             </li>
             <li>
-            Reserving a table is <b>$15</b> per person for 3 hours of gameplay and can be done at the top of the website.
+              $700 Birthday premium package. Have the entire backroom to yourselves with pizza, soda, unlimited bubble tea, unlimited appetizers and each participant gets a Sip & Play goodie bag! For ten people, each additional person is +$45
             </li>
-        </ul>
+          </ul>
+          <p>
+            <i>For any other large party inquiries, click the “Reserve Now!” button.</i>
+          </p>
+        </div>
+        <div className="vertical-box pricing">
+          <h1>PRICING</h1>
+          <ul>
+            <li>
+              <b>$10</b> per person for 3 hours (<b>$12</b> on weekends and holidays)
+            </li>
+            <li>
+              Reserving a table is <b>$15</b> per person for 3 hours of gameplay and can be done at the top of the website.
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
