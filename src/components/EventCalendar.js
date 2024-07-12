5e56a1654dd4d90123a725be1d114135b8ef54b7
Zian <hasanzian6@gmail.com>
2024-07-11 22:04:17 -0400

    Added Calendar and Footer

---
+++ src/components/EventCalendar.js
@@ -0,0 +1,50 @@
+
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './../assets/css/EventCalendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const EventCalendar = () => {
+  const [events, setEvents] = useState([
+    {
+      title: 'Dungeons and Dragons',
+      start: new Date(2024, 6, 12, 19, 0),
+      end: new Date(2024, 6, 12, 23, 0),
+    },
+    {
+      title: 'Chess Tournament',
+      start: new Date(2024, 6, 15, 12, 0),
+      end: new Date(2024, 6, 15, 18, 0),
+    },
+    {
+        title: 'Windmill Valley',
+        start: new Date(2024, 6, 25, 10, 0),
+        end: new Date(2024, 6, 25, 16, 0),
+    },
+    {
+        title: 'Rogue Seas',
+        start: new Date(2024, 6, 30, 1, 0),
+        end: new Date(2024, 6, 30, 19, 0),
+    },
+  ]);
+
+  return (
+    <div className="calendar-container">
+      <h2>Event Calendar</h2>
+      <div style={{ height: '500px' }}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: '100%' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default EventCalendar;
---
+++ src/components/Footer.js
@@ -0,0 +1,55 @@
+
+import React from 'react';
+import './../assets/css/Footer.css';
+
+import instagramLogo from './../assets/images/instagram-logo.png';
+import tiktokLogo from './../assets/images/tiktok-logo.png';
+import facebookLogo from './../assets/images/facebook-logo.png';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-left">
+          <div className="hours">
+            <p><b>Hours</b></p>
+            <p>Sunday: 10am-11pm</p>
+            <p>Mon-Thurs: 11am-11pm</p>
+            <p>Fri: 11am-midnight</p>
+            <p>Sat: 10am-midnight</p>
+            <p style={{ fontSize: "small" }}><b>Our Kitchen closes 2.5-3 hours before we close!</b></p>
+          </div>
+        </div>
+        <div className="footer-center">
+          <div className="address">
+            <p>471 5th Ave, Brooklyn, NY 11215, USA</p>
+          </div>
+          <div className="email">
+            <p>sipnplaynyc@gmail.com</p>
+          </div>
+          <div className="copyright">
+            <p>&copy; 2020 by Sip & Play</p>
+          </div>
+        </div>
+        <div className="footer-right">
+          <div className="social-media-icons">
+            <a href="https://www.instagram.com/sipnplaynyc/?hl=en" className="social-icon">
+              <img src={instagramLogo} alt="Instagram" />
+            </a>
+            <a href="https://www.tiktok.com/@sipnplaynycofficial" className="social-icon">
+              <img src={tiktokLogo} alt="Tiktok" />
+            </a>
+            <a href="https://www.facebook.com/sipnplaynyc/" className="social-icon">
+              <img src={facebookLogo} alt="Facebook" />
+            </a>
+            <div className="reserve-button">
+                <button className="reserve-btn" style={{ fontSize: "large" }}>Reserve Now!</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
